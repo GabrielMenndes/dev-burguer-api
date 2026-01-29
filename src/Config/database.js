df9215d3e6cd27b9660251d4ee37bb8494ab0739
GabrielMendes <gabrielmendes.dev@outlook.com>
2026-01-29 17:21:11 -0300

    api

---
+++ src/Config/database.js
@@ -0,0 +1,16 @@
+const { DatabaseError } = require("pg");
+const { use } = require("react");
+
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    username: 'admin',
+    password: '123456',
+    Database: 'dev-burguer-db',
+    define: {
+        timestamps: true,
+        underscored: true,
+        underscoredAll: true,
+    },
+}
