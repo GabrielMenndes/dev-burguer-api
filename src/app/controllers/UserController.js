d7e0590dec42dc071694a45d13bca30ef91143ae
GabrielMendes <gabrielmendes.dev@outlook.com>
2026-01-29 20:34:23 -0300

    att

---
+++ src/app/controllers/UserController.js
@@ -0,0 +1,34 @@
+import { v4 } from 'uuid';
+import User from '../models/User.js';
+
+class UserController {
+  async store(req, res) {
+    const { name, email, password_hash, admin } = req.body;
+
+    const existingUser = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    const user = await User.create({
+      id: v4(),
+      name,
+      email,
+      password_hash,
+      admin,
+    });
+    return res.status(201).json({
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      admin: user.admin,
+    });
+  }
+}
+
+export default new UserController();
---
+++ src/app/models/User.js
@@ -0,0 +1,22 @@
+import Sequelize, { Model } from 'sequelize';
+
+class User extends Model {
+  static init(sequelize) {
+    Model.init(
+      {
+        name: Sequelize.STRING,
+        email: Sequelize.STRING,
+        password_hash: Sequelize.STRING,
+        admin: {
+          type: Sequelize.BOOLEAN,
+          defaultValue: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: 'users',
+      },
+    );
+  }
+}
+export default User;
